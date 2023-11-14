6c27d7626649acdbe137cf75244a8662d60396a8
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-11-14 13:27:08 +0800

    Update Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,25 +1,26 @@
 <template>
     <input type="text" v-model="searchText" placeholder="搜索项目">
 </template>
-  
+
 <script>
 export default {
   name: "SearchBar",
-  props: ["value"],
+  props: ["value"], // 确保父组件传递了 value 属性
   data() {
-    // Remove searchText from data
-    return {};
+    return {
+      searchText: this.value, // 在组件的本地数据中声明 searchText
+    };
   },
-  computed: {
-    searchText: {
-      get() {
-        return this.value;
-      },
-      set(value) {
-        this.$emit("input", value);
-      },
-    },
+  watch: {
+    value(newVal) {
+      this.searchText = newVal; // 监听 value 的变化
+    }
   },
+  methods: {
+    updateValue(value) {
+      this.$emit("input", value); // 发出事件以更新父组件的值
+    }
+  }
 };
 </script>
 
@@ -32,4 +33,3 @@ input {
   border-radius: 4px;
 }
 </style>
-  
